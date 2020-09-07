c8aa3b1109c225c0b30377eb472732baa4efdb2c
Daniel Albuquerque <bdappalmoxarifado@gmail.com>
2020-09-07 18:37:34 -0300

    update-img

--- models/Subgroup.js
+++ models/Subgroup.js
@@ -1,38 +1,104 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const aws = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
 
 //CRIANDO O DOCUMENTO - ANÁLOGIA A TABELA NO BANCO DE DADOS
 const Subgroup = new Schema({
+  //QRCODE
   qrcode: {
     type: String,
     lowercase: true,
     required: false,
   },
+  //IMAGEM
   image: {
     type: String,
-    required: false,
+    require: true,
+  },
+  //IMAGEM
+  key: {
+    type: String,
+    require: true,
+  },
+  //DESCRIÇÃO
+  description: {
+    type: String,
+    required: true,
   },
   group: {
     type: Schema.Types.ObjectId,
     ref: "groups",
     required: true,
+    index: true
   },
-  description: {
+  //DATA DE LANÇAMENTO
+  releaseDateOf: {
     type: String,
-    required: true,
+    //default: Date.now()
   },
-  date: {
-    type: String,
-    default: Date.now(),
+  //USUARIO LANÇAMENTO
+  userLaunch: {
+    type: Schema.Types.ObjectId,
+    ref: "collaborators",
+    index: true
+    //required: true
   },
-  user: {
+  //EMAIL LANÇAMENTO
+  emailLaunch: {
     type: String,
-    default: "Daniel Soares de Albuquerque",
+    //default: Date.now()
+  },
+  //DATA DE EDIÇÃO
+  editionDate: {
+    type: String,
+    //default: Date.now()
+  },
+  //USUARIO DE EDIÇÃO
+  userEdtion: {
+    type: Schema.Types.ObjectId,
+    ref: "collaborators",
+    index: true
+    //required: true
+  },
+  //EMAIL DE EDIÇÃO
+  emailEdtion: {
+    type: String,
+    //required: true,
   },
   active: {
     type: Boolean,
     default: "true",
   },
+});
+
+Subgroup.pre("save", function () {
+  if (!this.image) {
+    this.image = `${process.env.APP_URL}/files/${this.key}`;
+  }
+});
+
+Subgroup.pre("remove", function () {
+  if (process.env.STORAGE_TYPE === "s3") {
+    return s3
+      .deleteObject({
+        Bucket: process.env.BUCKET_NAME,
+        Key: this.key
+      })
+      .promise()
+      .then(response => {
+        console.log(response.status);
+      })
+      .catch(response => {
+        console.log(response.status);
+      });
+  } else {
+    return promisify(fs.unlink)(
+      path.resolve(__dirname, "..", "tmp", "uploads", this.key)
+    );
+  }
 });
 
 mongoose.model("subgroups", Subgroup);
