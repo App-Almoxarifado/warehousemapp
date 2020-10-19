9e0a1552cf08f16647d360194a22406b61866f25
Daniel Soares de Albuquerque <64704136+App-Almoxarifado@users.noreply.github.com>
2020-10-19 18:15:36 -0300

    ok

--- routes/planning-route.js
+++ routes/planning-route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/planning-controller");
+const { eAdmin } = require("../helpers/eAdmin");
+const { eDevAdmin } = require("../helpers/eDevAdmin");
 
 //PRODUTOS - ROTA CARRINHO DE COMPRAS
-router.get("/", controller.request);
+router.get("/",eAdmin, controller.request);
 router.get("/cart/:id?", controller.getCart);
 router.post("/addItem", controller.postRequest);
 router.post("/updateItem", controller.updateRequest);
