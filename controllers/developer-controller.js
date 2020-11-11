3ba2139ced9fb3c3023534652c427679caf45b1d
Daniel Soares de Albuquerque <64704136+App-Almoxarifado@users.noreply.github.com>
2020-11-11 17:15:03 -0300

    cep

--- controllers/developer-controller.js
+++ controllers/developer-controller.js
@@ -2,9 +2,9 @@ const CepCoords = require("coordenadas-do-cep");
 
 exports.getIndex = async (req, res) => {
   try {
-    const info = await CepCoords.getByCep("72914093");
+    const info = await CepCoords.getByCep("14806500");
     console.log(info)
-    res.render("developers/developers", {info});
+    res.render("developers/cep", {info});
   } catch (err) {
     req.flash("error_msg", "Ops, Houve um erro interno!");
     res.redirect("/");
