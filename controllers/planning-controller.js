c8495cbcc96a38f2d6dabfa95d113a40c4105b88
Daniel Albuquerque <bdappalmoxarifado@gmail.com>
2020-11-27 03:17:22 -0300

    request

--- controllers/planning-controller.js
+++ controllers/planning-controller.js
@@ -32,191 +32,245 @@ const Planning = mongoose.model("plannings");
 exports.search = async (req, res) => {
   try {
     const warehouse = req.params._id;
-    const siteNow = await Warehouse.findOne({ _id: req.params._id })
-      .lean()
-      .populate("site");
-    
-    const warehouses = await Warehouse.find({ active: true })
-      .sort({ description: "asc" })
-      .lean();
-    /*
-    if(req.user.admin)
-      warehouses = await Warehouse.find({ active: true })
-      .sort({ description: "asc" })
-      .lean();
-    else warehouses = req.user.sites;
-    */
-    const groups = await Group.find({ active: true })
-      .sort({ description: "asc" })
-      .lean();
-
-    const subgroups = await Subgroup.find({ active: true })
-      .sort({ description: "asc" })
-      .lean();
-
-    const types = await Type.find({ active: true })
-      .sort({ description: "asc" })
-      .lean();
-
-    const statuses = await Status.find({ active: true })
-      .sort({ description: "asc" })
-      .lean();
-
-    const filtros = {
-      $or: [],
-      $and: [],
-    };
-
-    let {
-      search,
-      page,
-      site,
-      group,
-      subgroup,
-      type,
-      status,
-      limit,
-    } = req.query;
-
-    if (!!search) {
-      const pattern = new RegExp(`.*${search}.*`);
-      filtros["$or"].push(
-        { description: { $regex: pattern, $options: "i" } },
-        { fullDescription: { $regex: pattern, $options: "i" } },
-        { tag: { $regex: pattern, $options: "i" } },
-        { tagSearch: { $regex: pattern, $options: "i" } }
-      );
-    }
-
-
-    if(!!site) filtros["$and"].push({ warehouse: mongoose.Types.ObjectId(site) });
-    if (!!group) filtros["$and"].push({ group: mongoose.Types.ObjectId(group) });
-    if (!!subgroup) filtros["$and"].push({ subgroup:mongoose.Types.ObjectId(subgroup) });
-    if (!!type) filtros["$and"].push({ type: mongoose.Types.ObjectId(type) });
-    if (!!status) filtros["$and"].push({ status: mongoose.Types.ObjectId(status) });
-
-    if (filtros["$and"].length === 0) delete filtros["$and"];
-    if (filtros["$or"].length === 0) delete filtros["$or"];
-
-    const siteProducts = await Product.aggregate([
-      { $match: {"warehouse": mongoose.Types.ObjectId(warehouse)} },
-      { $group: {
-        "_id": "$tag",
-        "stock": { $sum: "$qtyStock"},
-        "request": { $sum: "$qtyRequest"}
-      }}
-    ]);
-
-    page = Number(page || 1);
-    limit = limit ? Number(limit) : 10;
-
-    const quant = await Product.find(filtros).estimatedDocumentCount();
-
-    /* var products = await Product.find(filtros)
-      .sort({
-        editionDate: "desc",
-      })
-      .limit(limit)
-      .lean()
-      .skip(page > 1 ? (page - 1) * limit : 0)
-      .populate("group")
-      .populate("subgroup")
-      .populate("type")
-      .populate("status");*/
-
-      console.log(filtros)
-      const products = await Product.aggregate([
-        {$match: filtros},
-        {$skip: page > 1 ? (page - 1) * limit : 0},
-        {$limit:limit},
-        {$sort:{tag:1}},
-        {$group: { 
-            _id: {
-              //type:"$type",
-              tag:"$tag",
-              group:"$group",
-              subgroup:"$subgroup",
-              //warehouse:"$warehouse",
-              description:"$description",
+      const siteNow = await Warehouse.findOne({ _id: req.params._id })
+        .lean()
+        .populate("site");
+  
+      const warehouses = await Warehouse.find({ active: true })
+        .sort({ description: "asc" })
+        .lean();
+      /*
+      if(req.user.admin)
+        warehouses = await Warehouse.find({ active: true })
+        .sort({ description: "asc" })
+        .lean();
+      else warehouses = req.user.sites;
+      */
+      const groups = await Group.find({ active: true })
+        .sort({ description: "asc" })
+        .lean();
+  
+      const subgroups = await Subgroup.find({ active: true })
+        .sort({ description: "asc" })
+        .lean();
+  
+      const types = await Type.find({ active: true })
+        .sort({ description: "asc" })
+        .lean();
+  
+      const statuses = await Status.find({ active: true })
+        .sort({ description: "asc" })
+        .lean();
+  
+      const filtros = {
+        $or: [],
+        $and: [],
+      };
+  
+      let {
+        search,
+        page,
+        site,
+        group,
+        subgroup,
+        type,
+        status,
+        limit,
+      } = req.query;
+  
+      if (!!search) {
+        const pattern = new RegExp(`.*${search}.*`);
+        filtros["$or"].push(
+          { description: { $regex: pattern, $options: "i" } },
+          { fullDescription: { $regex: pattern, $options: "i" } },
+          { tag: { $regex: pattern, $options: "i" } },
+          { user: { $regex: pattern, $options: "i" } }
+        );
+      }
+  
+      if (!!site)
+        filtros["$and"].push({ warehouse: mongoose.Types.ObjectId(site) });
+      if (!!group)
+        filtros["$and"].push({ group: mongoose.Types.ObjectId(group) });
+      if (!!subgroup)
+        filtros["$and"].push({ subgroup: mongoose.Types.ObjectId(subgroup) });
+      if (!!type) filtros["$and"].push({ type: mongoose.Types.ObjectId(type) });
+      if (!!status)
+        filtros["$and"].push({ status: mongoose.Types.ObjectId(status) });
+  
+      if (filtros["$and"].length === 0) delete filtros["$and"];
+      if (filtros["$or"].length === 0) delete filtros["$or"];
+  
+      //NO SITE FILTRADO
+      const siteProducts = await Product.aggregate([
+        { $match: { warehouse: mongoose.Types.ObjectId(warehouse) } },
+        {
+          $group: {
+            _id: "$tag",
+            stock: { $sum: "$qtyStock" },
+            request: { $sum: "$qtyReservation" },
+          },
+        },
+      ]);
+  
+      //TOTAL NA HBS
+      const totalProducts = await Product.aggregate([
+        {
+          $group: {
+            _id: "$tag",
+            stock: { $sum: "$qtyStock" },
+          },
+        },
+      ]);
+  
+      //PRODUTOS EM USO
+      const hbs = "5f4517adcb7b9f1394dbc002";
+      const useProducts = await Product.aggregate([
+        { $match: { warehouse: { $nin: [mongoose.Types.ObjectId(hbs)] } } },
+        {
+          $group: {
+            _id: "$tag",
+            stock: { $sum: "$qtyStock" },
+          },
+        },
+      ]);
+  
+      //PRODUTOS COM PENDÊNCIA
+      const bad1 = "5f01252e038643547805dbec";
+      const bad2 = "5f01252e038643547805dbed";
+      const badProducts = await Product.aggregate([
+        {
+          $match: {
+            status: {
+              $in: [mongoose.Types.ObjectId(bad1), mongoose.Types.ObjectId(bad2)],
             },
-             qtySite: { $sum: "$qtyStock"},
-             qtyRequest: { $sum: "$qtyRequest"},
-             qty: { $sum: "$qtyStock"},
-             qtyUse: { $sum: "$qtyStock"},
-             badAmount: { $sum: "$qtyStock"},
-             qtyHbs: { $sum: "$qtyStock"},
-          }
+          },
         },
         {
-          $lookup:
-          {
+          $group: {
+            _id: "$tag",
+            stock: { $sum: "$qtyStock" },
+          },
+        },
+      ]);
+  
+      //PRODUTOS EM USO
+      const centralHbs = "5f4517adcb7b9f1394dbc002";
+      const hbsProducts = await Product.aggregate([
+        { $match: { warehouse: { $in: [mongoose.Types.ObjectId(centralHbs)] } } },
+        {
+          $group: {
+            _id: "$tag",
+            stock: { $sum: "$qtyStock" },
+          },
+        },
+      ]);
+  
+      page = Number(page || 1);
+      limit = limit ? Number(limit) : 10;
+  
+      const quant = await Product.find(filtros).estimatedDocumentCount();
+      /* var products = await Product.find(filtros)
+        .sort({
+          editionDate: "desc",
+        })
+        .limit(limit)
+        .lean()
+        .skip(page > 1 ? (page - 1) * limit : 0)
+        .populate("group")
+        .populate("subgroup")
+        .populate("type")
+        .populate("status");*/
+  
+      console.log(filtros);
+      const products = await Product.aggregate([
+        { $match: filtros },
+        { $match: { active: { $in: [true] } } },
+        { $skip: page > 1 ? (page - 1) * limit : 0 },
+        { $limit: limit },
+        { $sort: { tag: 1 } },
+        {
+          $group: {
+            _id: {
+              type: "$type",
+              tag: "$tag",
+              group: "$group",
+              subgroup: "$subgroup",
+              description: "$description",
+            },
+          },
+        },
+        {
+          $lookup: {
             from: "groups",
             localField: "_id.group",
             foreignField: "_id",
-            as: "group"
-          }
+            as: "group",
+          },
         },
         { $unwind: "$group" },
         {
-          $lookup:
-          {
+          $lookup: {
             from: "subgroups",
             localField: "_id.subgroup",
             foreignField: "_id",
-            as: "subgroup"
-          }
+            as: "subgroup",
+          },
         },
         { $unwind: "$subgroup" },
-        /*{
-          $lookup:
-          {
+        {
+          $lookup: {
             from: "types",
             localField: "_id.type",
             foreignField: "_id",
-            as: "type"
-          }
+            as: "type",
+          },
         },
         { $unwind: "$type" },
-        {
-          $lookup:
-          {
-            from: "warehouses",
-            localField: "_id.warehouse",
-            foreignField: "_id",
-            as: "warehouse"
-          }
-        },
-        { $unwind: "$warehouse" },*/
-      ])
+        /*{
+            $lookup:
+            {
+              from: "warehouses",
+              localField: "_id.warehouse",
+              foreignField: "_id",
+              as: "warehouse"
+            }
+          },
+          { $unwind: "$warehouse" },*/
+      ]);
       // console.log(products)
-
-    res.render("planning/search", {
-      products,
-      prev: Number(page) > 1,
-      next: Number(page) * limit < quant,
-      warehouses,
-      groups,
-      subgroups,
-      types,
-      statuses,
-      page,
-      search,
-      limit,
-      site,
-      group,
-      subgroup,
-      type,
-      status,
-      siteNow,
-      warehouse,
-      siteProducts
-    });
-  } catch (err) {
-    console.log(err);
-    req.flash("error_msg", "Ops, Houve um erro interno!" + err);
-    res.redirect("/products");
-  }
-};
+  
+      res.render("planning/search", {
+        products,
+        prev: Number(page) > 1,
+        next: Number(page) * limit < quant,
+        warehouses,
+        groups,
+        subgroups,
+        types,
+        statuses,
+        page,
+        search,
+        limit,
+        site,
+        group,
+        subgroup,
+        type,
+        status,
+        siteNow,
+        warehouse,
+        siteProducts,
+        totalProducts,
+        useProducts,
+        badProducts,
+        hbsProducts,
+      });
+    } catch (err) {
+      console.log(err);
+      req.flash("error_msg", "Ops, Houve um erro interno!" + err);
+      res.redirect("planning/search");
+    }
+  };
 
 
 //SITES
